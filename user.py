7d24b48b9ce741c4745c743f9851e603bfa4ace4
Kanishk Mandwal <kanishkmandwal7@gmail.com>
2025-03-04 10:45:35 +0530

    [kanishk] add : python script to check valid last name

--- user.py
+++ user.py
@@ -11,6 +11,24 @@ def is_valid_first_name(name):
     else:
         print(f"{name} is an Invalid First Name.")
 
-# Example usage
+#function for checking the last name
+def is_valid_last_name(last_name):
+    """Validates if the last name starts with a capital letter and has a minimum of 3 characters."""
+    pattern = r'^[A-Z][a-zA-Z]{2,}$'
+    
+    if re.match(pattern, last_name):
+        print(f"{last_name} is a Valid Last Name.")
+    else:
+        print(f"{last_name} is an Invalid Last Name.")
+
+
+# user input for first namme
 first_name = input("Enter your First Name: ")
 is_valid_first_name(first_name)
+
+#user imput for last name 
+last_name = input("Enter your Last Name: ")
+is_valid_last_name(last_name)
+
+
+
